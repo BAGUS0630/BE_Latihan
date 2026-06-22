7e1f2cac5789d2c17d6003831ee87728e0bfa307
BAGUS0630 <mbagus0111@gmail.com>
2026-06-22 15:51:47 +0700

    push cors

--- config/cors.go
+++ config/cors.go
@@ -3,6 +3,7 @@ package config
 var allowedOrigins = []string{
 	"http://localhost:5173",
 	"https://belatihan-production-989c.up.railway.app"
+	"https://my-fe-theta.vercel.app"
 }
 
 func GetAllowedOrigins() []string {
